9a96e1d239823630c3a08b83a1823ced2d18a35e
Özmen <zmen@DESKTOP-9MRQIIH>
2026-02-25 03:10:13 +0300

    Add project files.

---
+++ Arkitektur.Entity/Entities/Feature.cs
@@ -0,0 +1,13 @@
+﻿using Arkitektur.Entity.Entities.Common;
+
+namespace Arkitektur.Entity.Entities
+{
+    public class Feature : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string BackgroundImage { get; set; }
+        public string Icon { get; set; }
+
+    }
+}
---
+++ Arkitektur.Entity/Enums/AppointmentStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Arkitektur.Entity.Enums
+{
+    public enum AppointmentStatus
+    {
+        Pending = 0,
+        Approved = 1,
+        Canceled = 2,
+    }
+}
